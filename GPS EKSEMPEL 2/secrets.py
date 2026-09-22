f4e2513642bf3a666d2ac334156b956a523ef02c
nat-kea <nafo@ek.dk>
2026-09-22 11:33:22 +0200

    Update secrets.py

--- GPS EKSEMPEL 2/secrets.py
+++ GPS EKSEMPEL 2/secrets.py
@@ -4,5 +4,5 @@ PASSWORD = 'YOUR WIFI PASSWORD'
 
 # Server credentials
 ACCESS_TOKEN = 'YOUR ThingsBoard Device ACCESS TOKEN'
-SERVER_IP_ADDRESS = 'demo.thingsboard.io'
+SERVER_IP_ADDRESS = 'cloud.thingsboard.eu'
 
